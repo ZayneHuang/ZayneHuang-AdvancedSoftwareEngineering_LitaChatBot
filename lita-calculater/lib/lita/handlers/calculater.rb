f22f2dc7527e4fb2c67ed5b21ed6082428abf276
ZayneHuang <huangzeyuan@126.com>
2020-10-28 11:45:53 +0800

    add function of calculater

--- lita-calculater/lib/lita/handlers/calculater.rb
+++ lita-calculater/lib/lita/handlers/calculater.rb
@@ -1,7 +1,57 @@
 module Lita
   module Handlers
     class Calculater < Handler
-      # insert handler code here
+      route(
+        /^calculate\s+(-?[1-9]\d*)([-*\/+])(-?[1-9]\d*)$/i,
+        :respond_with_calc,
+        command: true,
+        help: { 'calculate a+\-\*\/b' => 'prints a +\-\*\/ b = result' }
+      )
+
+      def respond_with_calc(response)
+        # Read up on the Ruby MatchData class for more options
+        a = response.match_data.captures[0]
+        op = response.match_data.captures[1]
+        b = response.match_data.captures[2]
+        a = Integer(a)
+        b = Integer(b)
+
+        puts a
+        puts b
+        puts op
+
+        case op
+        when "+"
+            response.reply "#{a} + #{b} = #{add a b}"
+        when "-"
+            response.reply "#{a} - #{b} = #{minus a b}"
+        when "*"
+            response.reply "#{a} * #{b} = #{multiply a b}"
+        when "/"
+            response.reply "#{a} * #{b} = #{divide a b}"
+        end
+
+      end
+
+      def add(a, b)
+          return a + b
+      end
+
+      def minus(a, b)
+        return a - b
+      end
+
+      def multiply(a, b)
+        return a * b
+      end
+
+      def divide(a, b)
+        if a % b == 0 then
+          return a / b
+        else
+          return a.to_f / b.to_f
+        end
+      end
 
       Lita.register_handler(self)
     end
--- lita-calculater/spec/lita/handlers/calculater_spec.rb
+++ lita-calculater/spec/lita/handlers/calculater_spec.rb
@@ -1,4 +1,44 @@
 require "spec_helper"
 
 describe Lita::Handlers::Calculater, lita_handler: true do
+    let(:robot) { Lita::Robot.new(registry) }
+
+    subject { described_class.new(robot) }
+
+    describe 'routing' do
+        it { is_expected.to route('Lita calculate 2+2') }
+        it { is_expected.to route('Lita calculate 2-2') }
+        it { is_expected.to route('Lita calculate 2*2') }
+        it { is_expected.to route('Lita calculate 2/2') }
+
+        it { is_expected.to_not route('Lita calculate 2 -2') }
+        it { is_expected.to_not route('Lita calculate    22*2') }
+
+        it { is_expected.to_not route('Lita calculate 2.5-2') }
+        it { is_expected.to_not route('Lita calculate 2+2.5') }
+
+        it { is_expected.to_not route('Lita CalCulate 4+4') }
+
+        it { is_expected.to_not route('Lita calculate two+two') }
+        it { is_expected.to_not route('Lita calculate 1e4=1e4') }
+    end
+
+    describe 'functionality' do
+        describe ':calculate_expr' do
+        let(:a) { rand(1..100) }
+        let(:b) { rand{1..100} }
+
+        it 'returns result of the expression' do
+            actual = subject.calculate_expr a b
+            expected = a + b
+
+            expect(actual).to eq(expected)
+        end
+        end
+
+        it 'calculate expression when asked to' do
+            send_message 'Lita calculate 2+2'
+            expect(replies.last).to eq('2 + 2 = 4')
+        end
+    end
 end
